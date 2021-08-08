09b72869de46751712a1f3830478e2b9b04534f5
E-Vargo <vargoelliott@gmail.com>
2021-08-07 20:08:43 -0700

    commit tree 6651b436e52b22a8afd555be50dbe7e14e3e02fa

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+name_array = ["Sally", "Dustin", "Elijah", "Viktor", "Jacob", "Elizabeth", "Candy"]
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+    name_array.collect {|x| badge_maker(x)}
+end
+
+def assign_rooms(name_array)
+    name_array.each_with_index.map {|x, i| "Hello, #{x}! You'll be assigned to room #{i + 1}!"}
+end
+
+def printer(name_array)
+     batch_badge_creator(name_array).each {|x| puts x}
+     assign_rooms(name_array).each {|e| puts e}
+    
+end
